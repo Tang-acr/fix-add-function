630d954a34f7a56f1d55b3140c54be2c61e3bdf2
Jie <2667571545@qq.com>
2022-06-02 11:23:39 +0800

    改到乘号

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,9 @@
 public class Solution {
-    public static int chu(int a, int b) {
-        return a / b;
+    public static int cheng(int a, int b) {
+        return a * b;
     }
 
     public static void main(String[] args) {
-        System.out.println("1/1=" + chu(1, 1));
+        System.out.println("1*1=" + cheng(1, 1));
     }
 }
