52433cda63828d1a3a60cddcb64c164510b87179
Jie <2667571545@qq.com>
2022-06-02 10:34:22 +0800

    减号改到除号

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,9 @@
 public class Solution {
-    public static int add(int a, int b) {
+    public static int chu(int a, int b) {
         return a / b;
     }
 
     public static void main(String[] args) {
-        System.out.println("1+1=" + add(1, 1));
+        System.out.println("1/1=" + chu(1, 1));
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 public class SolutionTest {
     @Test
     public void addTest() {
-        Assertions.assertEquals(Solution.add(1, 1), 2);
-        Assertions.assertEquals(Solution.add(1, 2), 3);
-        Assertions.assertEquals(Solution.add(-1, 1), 0);
-        Assertions.assertEquals(Solution.add(Integer.MAX_VALUE, Integer.MIN_VALUE), -1);
+        Assertions.assertEquals(Solution.chu(1, 1), 2);
+        Assertions.assertEquals(Solution.chu(1, 2), 3);
+        Assertions.assertEquals(Solution.chu(-1, 1), 0);
+        Assertions.assertEquals(Solution.chu(Integer.MAX_VALUE, Integer.MIN_VALUE), -1);
     }
 }
